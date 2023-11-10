75de374fb5666f9e34b2c2996dfa5a9a70f9c57f
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-11-10 13:59:19 +0000

    Finish homepage

--- src/home.js
+++ src/home.js
@@ -1,35 +1,6 @@
-// {/* <div id="content">
-//     <div id="header">
-//         <h1>Garfield's Bulkers Paradise</h1>
-//         <span id="links">
-//             <p id="">Home</p>
-//             <p>Menu</p>
-//             <p>Contact</p>
-//         </span>
-//     </div>
-//     <div id="middle">
-//         <p>You know the rules of bodybuilding, eat big to get big.
-//             Garfield's Bulkers Paradise has meal options for all the fellow gymrats that are on a bulk.
-//             We serve foods high in calories but with the right macros to help you in your bodybuilding journey.
-//         </p>
-//         <img  id="homeImage" src="./buff-garfield.png">
-//         <ul>
-//             <li class="miniTitle">Hours</li>
-//             <li>Sunday: 10am - 6pm</li>
-//             <li>Monday-Friday: 8am - 10pm</li>
-//             <li>Saturday: 10am - 8pm</li>
-//             <br>
-//             <li class="miniTitle">Location</li>
-//             <li>711 Maple Street</li>
-//         </ul>
-//     </div>
-// </div> */}
-
 import { ps, content } from './header.js';
 
 const middle = document.createElement("div");
-const introduction = document.createElement("p")
-const img = document.createElement("img")
 
 const Home = () =>{
     console.log(ps);
@@ -37,11 +8,31 @@ const Home = () =>{
     ps[0].setAttribute("id","currentPage");
     content.appendChild(middle);
     middle.setAttribute("id","middle");
+    const introduction = document.createElement("p")
+    const img = document.createElement("img")
     middle.appendChild(introduction);
     introduction.textContent = "You know the rules of bodybuilding, eat big to get big. Garfield's Bulkers Paradise has meal options for all the fellow gymrats that are on a bulk. We serve foods high in calories but with the right macros to help you in your bodybuilding journey.";
     middle.appendChild(img);
     img.setAttribute("id","homeImage");
     img.setAttribute("src","./buff-garfield.png");
+    const ul = document.createElement("ul");
+    middle.appendChild(ul);
+    ul.appendChild(document.createElement("li"));
+    ul.appendChild(document.createElement("li"));
+    ul.appendChild(document.createElement("li"));
+    ul.appendChild(document.createElement("li"));
+    ul.appendChild(document.createElement("br"));
+    ul.appendChild(document.createElement("li"));
+    ul.appendChild(document.createElement("li"));
+    const lis = document.querySelectorAll("li");
+    lis[0].setAttribute("id","miniTitle");
+    lis[0].textContent = "Hours"
+    lis[1].textContent = "Sunday: 10am - 6pm"
+    lis[2].textContent = "Monday-Friday: 8am - 10pm"
+    lis[3].textContent = "Saturday: 10am - 8pm"
+    lis[4].setAttribute("id","miniTitle");
+    lis[4].textContent = "Location";
+    lis[5].textContent = "711 Maple Street";
 }
 
 export { Home };
